248f3cac50ab1009055b89f0b82ff0cb7b03bacf
daniar-17 <daniarnuramin1@gmail.com>
2023-04-10 07:27:29 +0700

    Add Certificate 2

---
+++ resources/views/menu/certificate2.blade.php
@@ -0,0 +1,243 @@
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-lg-8">
+            <div class="text-center mb-5">
+                <h1 class="mb-3 ff-secondary fw-semibold lh-base">My <span class="text-primary">Certificate</span></h1>
+            </div>
+        </div>
+        <!-- end col -->
+    </div>
+    <!-- end row -->
+
+    <div class="row align-items-center">
+        <div class="col-lg-6 col-sm-7 col-10 mx-auto">
+            <div>
+                <img src="{{asset('assets/images/file.png')}}" alt="" class="img-fluid">
+            </div>
+        </div>
+        <div class="col-lg-6">
+            <div class="card">
+                <div class="card-body">
+                    <!-- Swiper -->
+                    <div class="swiper pagination-fraction-swiper rounded">
+                        <div class="swiper-wrapper">
+                            <div class="swiper-slide">
+                                <a href="javascript:void(0);" class="link-success" data-bs-toggle="modal" data-bs-target=".BWA_MERN">
+                                    <img src="{{asset('assets/images/sertifikat/BWA_MERN.jpg')}}" alt="" class="img-fluid" />
+                                </a>
+                            </div>
+                            <div class="swiper-slide">
+                                <a href="javascript:void(0);" class="link-success" data-bs-toggle="modal" data-bs-target=".BWA_Ecommerce">
+                                    <img src="{{asset('assets/images/sertifikat/BWA_Ecommerce.jpg')}}" alt="" class="img-fluid" />
+                                </a>
+                            </div>
+                            <div class="swiper-slide">
+                                <a href="javascript:void(0);" class="link-success" data-bs-toggle="modal" data-bs-target=".Dicoding_Javascript">
+                                    <img src="{{asset('assets/images/sertifikat/Dicoding_Javascript.jpg')}}" alt="" class="img-fluid" />
+                                </a>
+                            </div>
+                            <div class="swiper-slide">
+                                <a href="javascript:void(0);" class="link-success" data-bs-toggle="modal" data-bs-target=".Dicoding_Devops">
+                                    <img src="{{asset('assets/images/sertifikat/Dicoding_Devops.jpg')}}" alt="" class="img-fluid" />
+                                </a>
+                            </div>
+                            <div class="swiper-slide">
+                                <a href="javascript:void(0);" class="link-success" data-bs-toggle="modal" data-bs-target=".Dicoding_AWS">
+                                    <img src="{{asset('assets/images/sertifikat/Dicoding_AWS.jpg')}}" alt="" class="img-fluid" />
+                                </a>
+                            </div>
+                            <div class="swiper-slide">
+                                <a href="javascript:void(0);" class="link-success" data-bs-toggle="modal" data-bs-target=".Rakamin_Backend">
+                                    <img src="{{asset('assets/images/sertifikat/Rakamin_Backend.jpg')}}" alt="" class="img-fluid" />
+                                </a>
+                            </div>
+                            <div class="swiper-slide">
+                                <a href="javascript:void(0);" class="link-success" data-bs-toggle="modal" data-bs-target=".Rakamin_Fullstack">
+                                    <img src="{{asset('assets/images/sertifikat/Rakamin_Fullstack.jpg')}}" alt="" class="img-fluid" />
+                                </a>
+                            </div>
+                            <div class="swiper-slide">
+                                <a href="javascript:void(0);" class="link-success" data-bs-toggle="modal" data-bs-target=".Hacktiv8">
+                                    <img src="{{asset('assets/images/sertifikat/Hacktiv8.jpg')}}" alt="" class="img-fluid" />
+                                </a>
+                            </div>
+                            <div class="swiper-slide">
+                                <a href="javascript:void(0);" class="link-success" data-bs-toggle="modal" data-bs-target=".BNSP">
+                                    <img src="{{asset('assets/images/sertifikat/BNSP.jpg')}}" alt="" class="img-fluid" />
+                                </a>
+                            </div>
+                        </div>
+                        <div class="swiper-button-next bg-white shadow"></div>
+                        <div class="swiper-button-prev bg-white shadow"></div>
+                        <div class="swiper-pagination"></div>
+                    </div>
+                </div><!-- end card-body -->
+            </div><!-- end card -->
+        </div>
+        <!-- end col -->
+    </div>
+    <!-- end row -->
+</div>
+<!-- end container -->
+
+<!-- Modals -->
+<div class="modal fade BWA_MERN" tabindex="-1" role="dialog" aria-labelledby="myExtraLargeModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="myExtraLargeModalLabel"><i class="ri-award-fill align-bottom me-1 text-primary"></i> Sertifikat</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <img class="rounded shadow" alt="200x200" width="100%" src="{{asset('assets/images/sertifikat/BWA_MERN.jpg')}}">
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-light" data-bs-dismiss="modal">Close</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Modals -->
+<div class="modal fade BWA_Ecommerce" tabindex="-1" role="dialog" aria-labelledby="myExtraLargeModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="myExtraLargeModalLabel"><i class="ri-award-fill align-bottom me-1 text-primary"></i> Sertifikat</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <img class="rounded shadow" alt="200x200" width="100%" src="{{asset('assets/images/sertifikat/BWA_Ecommerce.jpg')}}">
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-light" data-bs-dismiss="modal">Close</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Modals -->
+<div class="modal fade Dicoding_Javascript" tabindex="-1" role="dialog" aria-labelledby="myExtraLargeModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="myExtraLargeModalLabel"><i class="ri-award-fill align-bottom me-1 text-primary"></i> Sertifikat</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <img class="rounded shadow" alt="200x200" width="100%" src="{{asset('assets/images/sertifikat/Dicoding_Javascript.jpg')}}">
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-light" data-bs-dismiss="modal">Close</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Modals -->
+<div class="modal fade Dicoding_Devops" tabindex="-1" role="dialog" aria-labelledby="myExtraLargeModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="myExtraLargeModalLabel"><i class="ri-award-fill align-bottom me-1 text-primary"></i> Sertifikat</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <img class="rounded shadow" alt="200x200" width="100%" src="{{asset('assets/images/sertifikat/Dicoding_Devops.jpg')}}">
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-light" data-bs-dismiss="modal">Close</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Modals -->
+<div class="modal fade Dicoding_AWS" tabindex="-1" role="dialog" aria-labelledby="myExtraLargeModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="myExtraLargeModalLabel"><i class="ri-award-fill align-bottom me-1 text-primary"></i> Sertifikat</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <img class="rounded shadow" alt="200x200" width="100%" src="{{asset('assets/images/sertifikat/Dicoding_AWS.jpg')}}">
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-light" data-bs-dismiss="modal">Close</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Modals -->
+<div class="modal fade Rakamin_Backend" tabindex="-1" role="dialog" aria-labelledby="myExtraLargeModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="myExtraLargeModalLabel"><i class="ri-award-fill align-bottom me-1 text-primary"></i> Sertifikat</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <img class="rounded shadow" alt="200x200" width="100%" src="{{asset('assets/images/sertifikat/Rakamin_Backend.jpg')}}">
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-light" data-bs-dismiss="modal">Close</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Modals -->
+<div class="modal fade Rakamin_Fullstack" tabindex="-1" role="dialog" aria-labelledby="myExtraLargeModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="myExtraLargeModalLabel"><i class="ri-award-fill align-bottom me-1 text-primary"></i> Sertifikat</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <img class="rounded shadow" alt="200x200" width="100%" src="{{asset('assets/images/sertifikat/Rakamin_Fullstack.jpg')}}">
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-light" data-bs-dismiss="modal">Close</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Modals -->
+<div class="modal fade Hacktiv8" tabindex="-1" role="dialog" aria-labelledby="myExtraLargeModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="myExtraLargeModalLabel"><i class="ri-award-fill align-bottom me-1 text-primary"></i> Sertifikat</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <img class="rounded shadow" alt="200x200" width="100%" src="{{asset('assets/images/sertifikat/Hacktiv8.jpg')}}">
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-light" data-bs-dismiss="modal">Close</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Modals -->
+<div class="modal fade BNSP" tabindex="-1" role="dialog" aria-labelledby="myExtraLargeModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="myExtraLargeModalLabel"><i class="ri-award-fill align-bottom me-1 text-primary"></i> Sertifikat</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <img class="rounded shadow" alt="200x200" width="100%" src="{{asset('assets/images/sertifikat/BNSP.jpg')}}">
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-light" data-bs-dismiss="modal">Close</button>
+            </div>
+        </div>
+    </div>
+</div>
