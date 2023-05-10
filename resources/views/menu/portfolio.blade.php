9c7d465196fa228889792e2cc8bd1cca4527c0e1
daniar-17 <daniarnuramin1@gmail.com>
2023-05-10 11:22:54 +0700

    Add Portfolio PDF and Image html 2 canvas

--- resources/views/menu/portfolio.blade.php
+++ resources/views/menu/portfolio.blade.php
@@ -287,6 +287,27 @@
                 </div>
             </div>
         </div>
+        <div class="col-lg-4 col-md-6 product-item laravel">
+            <div class="card explore-box card-animate">
+                <div class="card-body">
+                    <img src="{{asset('assets/images/pdf_image/01_conv.png')}}" alt="" class="img-fluid rounded" />
+                </div>
+                <div class="card-body">
+                    <ul class="list-inline fs-14 text-muted">
+                        <li class="list-inline-item">
+                            <i class="ri-checkbox-circle-line align-bottom me-1"></i>PHP, Laravel, Canvas & PDF
+                        </li>
+                    </ul>
+                    <a href="javascript:void(0);">
+                        <h5>Generate PDF & Image</h5>
+                    </a>
+                    <p class="text-muted fs-14">This is a web application about Generate PDF to Send Email & HTML 2 Canvas.</p>
+                    <div>
+                        <a href="javascript:void(0);" class="link-success" data-bs-toggle="modal" data-bs-target=".pdf-image">Learn More <i class="ri-arrow-right-line align-bottom ms-1"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 
@@ -1631,6 +1652,122 @@
                                         </div>
                                         <div class="swiper-slide">
                                             <img src="{{asset('assets/images/simple_crud_posts/05_delete.png')}}" alt="" class="img-fluid" />
+                                        </div>
+                                    </div>
+                                    <div class="swiper-button-next bg-white shadow"></div>
+                                    <div class="swiper-button-prev bg-white shadow"></div>
+                                    <div class="swiper-pagination"></div>
+                                </div>
+                            </div><!-- end card-body -->
+                        </div><!-- end card -->
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-light" data-bs-dismiss="modal">Close</button>
+            </div>
+        </div><!-- /.modal-content -->
+    </div><!-- /.modal-dialog -->
+</div>
+
+<div class="modal fade pdf-image" tabindex="-1" role="dialog" aria-labelledby="myExtraLargeModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-xl">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="myExtraLargeModalLabel"><i class="ri-award-fill align-bottom me-1 text-primary"></i> PHP, Laravel & Canvas</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <div class="row">
+                    <div class="col-lg-5">
+                        <p class="fs-14 text-muted">
+                            This is a web application about Generate PDF to Send Email & HTML 2 Canvas. This application is made for self learning, this application has features : 
+                        </p>
+                        <hr class="text-muted">
+                        <div class="d-flex">
+                            <div class="flex-shrink-0">
+                                <i class="ri-checkbox-circle-fill text-primary"></i>
+                            </div>
+                            <div class="flex-grow-1 ms-2 fs-14">
+                                <p class="text-muted mb-0">Generate <strong>File Pdf.</strong></p>
+                            </div>
+                        </div>
+                        <div class="d-flex mt-2 mb-2">
+                            <div class="flex-shrink-0">
+                                <i class="ri-checkbox-circle-fill text-primary"></i>
+                            </div>
+                            <div class="flex-grow-1 ms-2 fs-14">
+                                <p class="text-muted mb-0">Send email <strong>according with Input.</strong></p>
+                            </div>
+                        </div>
+                        <div class="d-flex mt-2 mb-2">
+                            <div class="flex-shrink-0">
+                                <i class="ri-checkbox-circle-fill text-primary"></i>
+                            </div>
+                            <div class="flex-grow-1 ms-2 fs-14">
+                                <p class="text-muted mb-0">Send Email <strong>With attachment file Pdf from generate system.</strong></p>
+                            </div>
+                        </div>
+                        <div class="d-flex mt-2 mb-2">
+                            <div class="flex-shrink-0">
+                                <i class="ri-checkbox-circle-fill text-primary"></i>
+                            </div>
+                            <div class="flex-grow-1 ms-2 fs-14">
+                                <p class="text-muted mb-0"><strong>Generate / Screenshot</strong> page HTML to be image with html2canvas.</p>
+                            </div>
+                        </div>
+                        <div class="d-flex mt-2 mb-2">
+                            <div class="flex-shrink-0">
+                                <i class="ri-checkbox-circle-fill text-primary"></i>
+                            </div>
+                            <div class="flex-grow-1 ms-2 fs-14">
+                                <p class="text-muted mb-0">Button <strong>Set Text</strong> to according input text with image caption.</p>
+                            </div>
+                        </div>
+                        <div class="d-flex mt-2 mb-2">
+                            <div class="flex-shrink-0">
+                                <i class="ri-checkbox-circle-fill text-primary"></i>
+                            </div>
+                            <div class="flex-grow-1 ms-2 fs-14">
+                                <p class="text-muted mb-0">Button <strong>Generate</strong> to download final result.</p>
+                            </div>
+                        </div>
+                        <p class="mb-2">
+                            <span class="text-primary">Languages and Tools :</span> 
+                            <div class="hstack gap-2 mb-2">
+                                <span class="badge badge-soft-primary">PHP</span>
+                                <span class="badge badge-soft-primary">Laravel</span>
+                                <span class="badge badge-soft-primary">HTML 2 Canvas</span>
+                                <span class="badge badge-soft-primary">Generate PDF</span>
+                            </div>
+                        </p>
+                    </div>
+                    <div class="col-lg-7">
+                        <div class="card bg-soft-dark">
+                            <div class="card-body">
+                                <!-- Swiper -->
+                                <div class="swiper pagination-fraction-swiper rounded">
+                                    <div class="swiper-wrapper">
+                                        <div class="swiper-slide">
+                                            <img src="{{asset('assets/images/pdf_image/01_conv.png')}}" alt="" class="img-fluid" />
+                                        </div>
+                                        <div class="swiper-slide">
+                                            <img src="{{asset('assets/images/pdf_image/02_conv.png')}}" alt="" class="img-fluid" />
+                                        </div>
+                                        <div class="swiper-slide">
+                                            <img src="{{asset('assets/images/pdf_image/03_download.png')}}" alt="" class="img-fluid" />
+                                        </div>
+                                        <div class="swiper-slide">
+                                            <img src="{{asset('assets/images/pdf_image/04_result.png')}}" alt="" class="img-fluid" />
+                                        </div>
+                                        <div class="swiper-slide">
+                                            <img src="{{asset('assets/images/pdf_image/01_generate_pdf_01.png')}}" alt="" class="img-fluid" />
+                                        </div>
+                                        <div class="swiper-slide">
+                                            <img src="{{asset('assets/images/pdf_image/01_generate_multi2.png')}}" alt="" class="img-fluid" />
+                                        </div>
+                                        <div class="swiper-slide">
+                                            <img src="{{asset('assets/images/pdf_image/01_generate_multi.jpeg')}}" alt="" class="img-fluid" />
                                         </div>
                                     </div>
                                     <div class="swiper-button-next bg-white shadow"></div>
